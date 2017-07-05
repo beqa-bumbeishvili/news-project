ca81630f8486c61c8fffb6db1db75c24348a7a9a
beqa-bumbeishvili <bumbeishvili.beqa@gmail.com>
2017-07-05 15:50:26 +0400

    update urls in test controllers

--- test/controllers/news_versions_controller_test.rb
+++ test/controllers/news_versions_controller_test.rb
@@ -6,43 +6,43 @@ class NewsVersionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get index' do
-    get news_versions_url
+    get admin_news_versions_url
     assert_response :success
   end
 
   test 'should get new' do
-    get new_news_version_url
+    get new_admin_news_version_url
     assert_response :success
   end
 
   test 'should create news_version' do
     assert_difference('NewsVersion.count') do
-      post news_versions_url, params: { news_version: { content: @news_version.content, created_at: @news_version.created_at, news_id: @news_version.news_id, post_type_id: @news_version.post_type_id, published_at: @news_version.published_at, title: @news_version.title, updated_at: @news_version.updated_at } }
+      post admin_news_versions_url, params: { news_version: { content: @news_version.content, created_at: @news_version.created_at, news_id: @news_version.news_id, post_type_id: @news_version.post_type_id, published_at: @news_version.published_at, title: @news_version.title, updated_at: @news_version.updated_at } }
     end
 
-    assert_redirected_to news_version_url(NewsVersion.last)
+    assert_redirected_to admin_news_version_url(NewsVersion.last)
   end
 
   test 'should show news_version' do
-    get news_version_url(@news_version)
+    get admin_news_version_url(@news_version)
     assert_response :success
   end
 
   test 'should get edit' do
-    get edit_news_version_url(@news_version)
+    get edit_admin_news_version_url(@news_version)
     assert_response :success
   end
 
   test 'should update news_version' do
-    patch news_version_url(@news_version), params: { news_version: { content: @news_version.content, created_at: @news_version.created_at, news_id: @news_version.news_id, post_type_id: @news_version.post_type_id, published_at: @news_version.published_at, title: @news_version.title, updated_at: @news_version.updated_at } }
-    assert_redirected_to news_version_url(@news_version)
+    patch admin_news_version_url(@news_version), params: { news_version: { content: @news_version.content, created_at: @news_version.created_at, news_id: @news_version.news_id, post_type_id: @news_version.post_type_id, published_at: @news_version.published_at, title: @news_version.title, updated_at: @news_version.updated_at } }
+    assert_redirected_to admin_news_version_url(@news_version)
   end
 
   test 'should destroy news_version' do
     assert_difference('NewsVersion.count', -1) do
-      delete news_version_url(@news_version)
+      delete admin_news_version_url(@news_version)
     end
 
-    assert_redirected_to news_versions_url
+    assert_redirected_to admin_news_versions_url
   end
 end
--- test/models/news_test.rb
+++ test/models/news_test.rb
@@ -2,9 +2,6 @@ require 'test_helper'
 
 class NewsTest < ActiveSupport::TestCase
   test 'should create empty version' do
-    # News.order('RANDOM()').first
-    # version = NewsVersion.new(news_id: 10, active: false)
-    # p "ახლად შექმნილი ვერსია - #{version}"
-    # assert version.save!
+
   end
 end
